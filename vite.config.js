c447d1a674fd1b4d182404954f4748374ca53a6a
iWantToBeAProgrammer <juanstevenson10@gmail.com>
2024-11-14 22:44:00 +0700

    dockerd config

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.jsx",
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        host: true, // Mengizinkan akses dari semua host
-        port: 5173,
+  plugins: [
+    laravel({
+      input: ['resources/js/app.jsx'],
+      refresh: true, // Enables hot reload for Inertia
+    }),
+    react(),
+  ],
+  server: {
+    host: true, // Allows access from the Docker container
+    hmr: {
+      host: 'localhost', // Docker container hostname
     },
+  },
 });
